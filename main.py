9f99519118421e6f87442e75801d0eb64f5c0629
Nikitka32H <158262884+Nikitka32H@users.noreply.github.com>
2024-03-20 08:15:53 +0000

    add snail.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-# print(my name is "my_name")
-my_name="Jeanne d'Arc"
+a1 = 5
+a2 = 50
+a3 = 500
+print('a1=',5)
+print('a2=',50)
+print('a3=',500)
+
+b1 = 5
+b2 = 50
+b3 = 500
+print('b1=',5)
+print('b1=',50)
---
+++ snail.py
@@ -0,0 +1,5 @@
+b=10
+a=15
+h=int(input("input h="))
+days=(h-2*b+a-1)//(a-b)
+print("days=",days)
